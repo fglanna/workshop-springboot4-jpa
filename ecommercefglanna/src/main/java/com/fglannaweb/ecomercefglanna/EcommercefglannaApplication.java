f40eac124d3dff2e255a76b4fb99149c8bb01ae6
FLAVIO GOMES DE LANNA <132150354+fglanna@users.noreply.github.com>
2026-06-26 13:13:20 -0300

    Project created

---
+++ ecommercefglanna/src/main/java/com/fglannaweb/ecomercefglanna/EcommercefglannaApplication.java
@@ -0,0 +1,13 @@
+package com.fglannaweb.ecomercefglanna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercefglannaApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(EcommercefglannaApplication.class, args);
+    }
+
+}
