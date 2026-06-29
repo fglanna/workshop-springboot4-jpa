e32423aef66e50378cf9dfe80fa09d93efeb8e90
FLAVIO GOMES DE LANNA <132150354+fglanna@users.noreply.github.com>
2026-06-29 14:33:24 -0300

    Category created

---
+++ ecommercefglanna/src/main/java/com/fglannaweb/ecomercefglanna/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.fglannaweb.ecomercefglanna.repositories;
+
+import com.fglannaweb.ecomercefglanna.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
