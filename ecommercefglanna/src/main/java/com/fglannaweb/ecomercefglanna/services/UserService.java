7a8541da7fefa8a0f845920c57fe399630c3cfaa
FLAVIO GOMES DE LANNA <132150354+fglanna@users.noreply.github.com>
2026-07-02 14:20:02 -0300

    Exception handling - findById

--- ecommercefglanna/src/main/java/com/fglannaweb/ecomercefglanna/services/UserService.java
+++ ecommercefglanna/src/main/java/com/fglannaweb/ecomercefglanna/services/UserService.java
@@ -2,6 +2,7 @@ package com.fglannaweb.ecomercefglanna.services;
 
 import com.fglannaweb.ecomercefglanna.entities.User;
 import com.fglannaweb.ecomercefglanna.repositories.UserRepository;
+import com.fglannaweb.ecomercefglanna.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
     }
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
